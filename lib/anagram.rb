90543bac6bf27cba54ca8de9c038af14e5e30514
Michael Martinez <zeroeclipse@live.com>
2020-10-31 20:50:24 -0700

    commit tree ef4b50415d9833baa23180cb049b433c1aab338e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word_array
+ 
+    def initialize(words)
+         @letter_array = words.split("")
+    end
+ 
+    def match(strings)
+     final_array = []
+
+     strings.each do |string|
+       temp_array = string.split("")
+       if temp_array.sort == @letter_array.sort
+         final_array.push(string)
+       end
+     end
+     return final_array
+    end
+ end
